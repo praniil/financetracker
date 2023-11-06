8f94489bcc73f7661fcd36428d78d0c801032aa8
praniil <pparajuli15@gmail.com>
2023-11-06 12:04:16 +0545

    update: updated the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import AccBalance from './Frontend/Components/AccountBalance/AccBalance';
+import Stats from './Frontend/Components/ExpenseStructure/Stats';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AccBalance/>
+      <Stats/>
     </div>
   );
 }
