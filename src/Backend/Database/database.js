56c08569bc31642c69b2f5a2c2873359215362f1
praniil <pparajuli15@gmail.com>
2023-11-06 12:03:31 +0545

    feat: added backend to the project

---
+++ src/Backend/Database/database.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const { Pool } = require('pg')
+const port = 8080;
+
+
+
+const app = express();
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '206001',
+    port: 5432
+});
+
+app.use(express.json());
+
+async function financeBalance() {
+    try {
+        const query = `
+        CREATE TABLE IF NOT EXISTS finbalance(
+            id SERIAL PRIMARY KEY,
+            balance NUMERIC(10, 2)
+        )
+        `;
+        await pool.query(query);
+        console.log('finbalance table created');
+    } catch (error) {
+        console.log(error);
+        console.error('Financial Balance Table creation failed')
+    }
+}
+
+financeBalance();
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
