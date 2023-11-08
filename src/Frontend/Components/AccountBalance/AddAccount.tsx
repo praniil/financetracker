78356021ffe81c8cd05c2299d1bdb63f3af52430
praniil <pparajuli15@gmail.com>
2023-11-08 15:47:51 +0545

    feat: added select menu in addAccount.tsx

--- src/Frontend/Components/AccountBalance/AddAccount.tsx
+++ src/Frontend/Components/AccountBalance/AddAccount.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
 
+const optionArray: string[] = [
+  "General",
+  "Cash",
+  "Current account",
+  "Saving account",
+  "Loan",
+];
+const currencytype : string[] = [
+    "NPR",
+    "USD",
+    "CAD",
+    "INR"
+]
 const AddAccount = () => {
   return (
     <div>
-      i am add accoutnt
+      <input
+        type="text"
+        name="accname"
+        id="accname"
+        placeholder="Account Name"
+      />
+      <input type="text" name="initialbalance" placeholder="Initial Balance" />
+      <select name="accounttype" id="accounttype">
+        {optionArray.map((items, index) => (
+          <option key={index}>{items}</option>
+        ))}
+      </select>
+      <select name="currencytype" id="currencytype">
+            {currencytype.map((currency, index) => (
+                <option key={index}>{currency}</option>
+            ))}
+      </select>
+      
     </div>
-  )
-}
+  );
+};
 
-export default AddAccount
+export default AddAccount;
