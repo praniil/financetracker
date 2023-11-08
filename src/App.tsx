22849775737719e86d6afc3d03ffde7b83de3ba1
praniil <pparajuli15@gmail.com>
2023-11-08 20:35:44 +0545

    update: updated main accBalance.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import AccBalance from "./Frontend/Components/AccountBalance/AccBalance";
 import Stats from "./Frontend/Components/ExpenseStructure/Stats";
 import AddAccount from "./Frontend/Components/AccountBalance/AddAccount";
 import Navbar from "./Frontend/Navbar/Navbar";
+import Template from "./Frontend/Components/AccountBalance/Template";
+import NewRecord from "./Frontend/Components/AccountBalance/NewRecord";
 
 function App() {
   return (
@@ -15,7 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<AccBalance />} />
           {/* <Stats/> */}
-          <Route path="/addAccount" element={<AddAccount />} />
+          <Route path="/addAccount" element={<AddAccount/>} />
+          <Route path="/template" element={<Template/>} />
+          <Route path="/newRecord" element={<NewRecord/>} />
+
         </Routes>
       </div>
     </BrowserRouter>
--- src/Frontend/Components/AccountBalance/AccBalance.tsx
+++ src/Frontend/Components/AccountBalance/AccBalance.tsx
@@ -8,9 +8,14 @@ const AccBalance = () => {
   const [isAdjustingBalance, setIsAdjustingBalance] = useState<boolean>(false);
   const [newBalance, setNewBalance] = useState<number | null>(null);
   const [isDropdown, setIsDropdown] = useState<boolean>(false);
+  const [isNewRecordDown, setIsNewRecordDown] = useState<boolean>(false);
 
+  //dropdown
   function toggleDropdown() {
     setIsDropdown(!isDropdown);
+  }
+  function toggleRecordDropdown() {
+    setIsNewRecordDown(!isNewRecordDown);
   }
   function handleBalance() {
     setIsAdjustingBalance(!isAdjustingBalance);
@@ -74,7 +79,7 @@ const AccBalance = () => {
         ) : (
           <button
             onClick={handleBalance}
-            className="bg-slate-500 text-white rounded-sm p-1 m-1"
+            className="bg-slate-500 text-white rounded-md p-1 m-1"
           >
             {" "}
             Adjust Balance{" "}
@@ -86,7 +91,7 @@ const AccBalance = () => {
       <button className="bg-blue-500 text-white rounded-sm p-1 m-1" onClick={toggleDropdown}>Add Account</button>} */}
       <div className="flex flex-col">
         <button
-          className="bg-slate-500 text-white rounded-md p-1 m-auto w-32 "
+          className="bg-slate-500 text-white rounded-md p-1 m-auto w-32"
           onClick={toggleDropdown}
         >
           Add Account
@@ -107,11 +112,33 @@ const AccBalance = () => {
                 <li>
                   <Link to="#">Bank Sync</Link>
                   <p className="font-serif text-sm text-slate-500">
-                    Connect to your bank account. Synchronise your transactions to Wallet automatically.
+                    Connect to your bank account. Synchronise your transactions
+                    to Wallet automatically.
                   </p>
                 </li>
               </ul>
             </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <button
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0 m-10"
+          onClick={toggleRecordDropdown}
+        >
+          +
+        </button>
+
+        {isNewRecordDown && (
+          <div>
+            <ul className="absolute bottom-20 right-24">
+              <Link to="/template">
+                <li>Template</li>
+              </Link>
+              <Link to="/newRecord">
+                <li>New Record</li>
+              </Link>
+            </ul>
           </div>
         )}
       </div>
