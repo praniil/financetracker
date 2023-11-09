7c99da45a370a68f4f320d26bd7b2385ab308ed5
praniil <pparajuli15@gmail.com>
2023-11-09 22:40:12 +0545

    update: updated createContext hook

--- src/Frontend/Components/AccountBalance/AccBalance.tsx
+++ src/Frontend/Components/AccountBalance/AccBalance.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-const AccBalance = () => {
+interface ProfileProps {
+  balance: number;
+  setBalance: React.Dispatch<React.SetStateAction<number>>;
+}
+interface AccBalanceProps {
+  children: React.ReactNode; // Define children prop
+}
+const ProfileContext = createContext<ProfileProps>({
+  balance: 0,
+  setBalance: () => {},
+});
+
+const AccBalance: React.FC<AccBalanceProps> = ({ children }) => {
   //useStates
   const [balance, setBalance] = useState<number>(0);
   const [isAdjustingBalance, setIsAdjustingBalance] = useState<boolean>(false);
@@ -54,96 +66,99 @@ const AccBalance = () => {
   function handleAddacc() {}
   return (
     <div>
-      <div className="flex justify-center space-x-4">
-        <div className="m-1 p-1">Cash: {balance}</div>
+      <ProfileContext.Provider value={{ balance, setBalance }}>
+        {children}
+        <div className="flex justify-center space-x-4">
+          <div className="m-1 p-1">Cash: {balance}</div>
 
-        {isAdjustingBalance ? (
-          <div>
-            <input
-              type="text"
-              className="border-blue-300"
-              value={newBalance || ""}
-              onChange={(event) =>
-                handleBalanceChange(Number(event.target.value))
-              }
-              onKeyDown={handleKeyPress}
-            />
+          {isAdjustingBalance ? (
+            <div>
+              <input
+                type="text"
+                className="border-blue-300"
+                value={newBalance || ""}
+                onChange={(event) =>
+                  handleBalanceChange(Number(event.target.value))
+                }
+                onKeyDown={handleKeyPress}
+              />
+              <button
+                onClick={handleSaveBalance}
+                className="bg-blue-500 text-white rounded-sm p-1 m-1"
+              >
+                {" "}
+                Done{" "}
+              </button>
+            </div>
+          ) : (
             <button
-              onClick={handleSaveBalance}
-              className="bg-blue-500 text-white rounded-sm p-1 m-1"
+              onClick={handleBalance}
+              className="bg-slate-500 text-white rounded-md p-1 m-1"
             >
               {" "}
-              Done{" "}
+              Adjust Balance{" "}
             </button>
-          </div>
-        ) : ( 
-          <button
-            onClick={handleBalance}
-            className="bg-slate-500 text-white rounded-md p-1 m-1"
-          >
-            {" "}
-            Adjust Balance{" "}
-          </button>
-        )}
-      </div>
-      {/* {
+          )}
+        </div>
+        {/* {
         isDropdown? ()
       <button className="bg-blue-500 text-white rounded-sm p-1 m-1" onClick={toggleDropdown}>Add Account</button>} */}
-      <div className="flex flex-col">
-        <button
-          className="bg-slate-500 text-white rounded-md p-1 m-auto w-32"
-          onClick={toggleDropdown}
-        >
-          Add Account
-        </button>
-        {isDropdown && (
-          <div>
-            <div className="bg-white-500 text-black rounded-sm p-1 m-auto border-2 border-red-300 w-96">
-              {" "}
-              <ul>
-                <li>
-                  <Link to="/addaccount">Manual Input </Link>
-                  <p className="font-serif text-sm text-slate-500">
-                    Update your account manually. You can import your
-                    transaction or connect the account to your bank later, if
-                    you wish.
-                  </p>
-                </li>
-                <li>
-                  <Link to="#">Bank Sync</Link>
-                  <p className="font-serif text-sm text-slate-500">
-                    Connect to your bank account. Synchronise your transactions
-                    to Wallet automatically.
-                  </p>
-                </li>
+        <div className="flex flex-col">
+          <button
+            className="bg-slate-500 text-white rounded-md p-1 m-auto w-32"
+            onClick={toggleDropdown}
+          >
+            Add Account
+          </button>
+          {isDropdown && (
+            <div>
+              <div className="bg-white-500 text-black rounded-sm p-1 m-auto border-2 border-red-300 w-96">
+                {" "}
+                <ul>
+                  <li>
+                    <Link to="/addaccount">Manual Input </Link>
+                    <p className="font-serif text-sm text-slate-500">
+                      Update your account manually. You can import your
+                      transaction or connect the account to your bank later, if
+                      you wish.
+                    </p>
+                  </li>
+                  <li>
+                    <Link to="#">Bank Sync</Link>
+                    <p className="font-serif text-sm text-slate-500">
+                      Connect to your bank account. Synchronise your
+                      transactions to Wallet automatically.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+        <div>
+          <button
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0 m-10"
+            onClick={toggleRecordDropdown}
+          >
+            +
+          </button>
+
+          {isNewRecordDown && (
+            <div>
+              <ul className="absolute bottom-20 right-24">
+                <Link to="/template">
+                  <li>Template</li>
+                </Link>
+                <Link to="/newRecord">
+                  <li>New Record</li>
+                </Link>
               </ul>
             </div>
-          </div>
-        )}
-      </div>
-      <div>
-        <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0 m-10"
-          onClick={toggleRecordDropdown}
-        >
-          +
-        </button>
-
-        {isNewRecordDown && (
-          <div>
-            <ul className="absolute bottom-20 right-24">
-              <Link to="/template">
-                <li>Template</li>
-              </Link>
-              <Link to="/newRecord">
-                <li>New Record</li>
-              </Link>
-            </ul>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </ProfileContext.Provider>
     </div>
   );
 };
-
+export { ProfileContext };
 export default AccBalance;
