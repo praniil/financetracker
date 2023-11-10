386c285930aecf94bccfb400d42c99f67fb4d83f
praniil <pparajuli15@gmail.com>
2023-11-10 17:06:18 +0545

    feat: added a state props to AccBalance and NewRecord

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AccBalance from "./Frontend/Components/AccountBalance/AccBalance";
@@ -10,15 +11,16 @@ import Template from "./Frontend/Components/AccountBalance/Template";
 import NewRecord from "./Frontend/Components/AccountBalance/NewRecord";
 
 function App() {
+  const [balance, setBalance] = useState<number>(0);
   return (
     <BrowserRouter>
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<AccBalance />} />
-          <Route path="/addAccount" element={<AddAccount />} />
+          <Route path="/" element={<AccBalance passBalance = {{balance, setBalance}}/>} />
+          <Route path="/addAccount" element={<AddAccount  />} />
           <Route path="/template" element={<Template />} />
-          <Route path="/newRecord" element={<NewRecord />} />
+          <Route path="/newRecord" element={<NewRecord passBalance = {{balance, setBalance}}/>} />
         </Routes>
       </div>
     </BrowserRouter>
