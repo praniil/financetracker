83fca7d9f44f3ff651d425803de1d4b2472aaff8
praniil <pparajuli15@gmail.com>
2023-11-10 16:22:37 +0545

    feat: added a async function to get data from backend

--- src/Frontend/Components/AccountBalance/AccBalance.tsx
+++ src/Frontend/Components/AccountBalance/AccBalance.tsx
@@ -1,26 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-interface ProfileProps {
-  balance: number;
-  setBalance: React.Dispatch<React.SetStateAction<number>>;
-}
-interface AccBalanceProps {
-  children: React.ReactNode; // Define children prop
-}
-const ProfileContext = createContext<ProfileProps>({
-  balance: 0,
-  setBalance: () => {},
-});
-
-const AccBalance: React.FC<AccBalanceProps> = ({ children }) => {
+const AccBalance: React.FC = () => {
   //useStates
   const [balance, setBalance] = useState<number>(0);
   const [isAdjustingBalance, setIsAdjustingBalance] = useState<boolean>(false);
   const [newBalance, setNewBalance] = useState<number | null>(null);
   const [isDropdown, setIsDropdown] = useState<boolean>(false);
   const [isNewRecordDown, setIsNewRecordDown] = useState<boolean>(false);
+
+  const fetchUpdateBalance = async () => {
+    console.log("in fetchUpdateBalance function");
+    try {
+      const response = await axios.get("http://localhost:8080/api/get-balance");
+      const updatedBalance = response.data.balance;
+      console.log("updatedBalance", updatedBalance);
+      setBalance(updatedBalance);
+    } catch (error) {
+      console.error("Error fetching updated balance: ", error);
+    }
+  };
+  //empty array in useeffect : runs only once after the initial render
+  useEffect(() => {
+    fetchUpdateBalance();
+  }, []);
 
   //dropdown
   function toggleDropdown() {
@@ -47,7 +51,7 @@ const AccBalance: React.FC<AccBalanceProps> = ({ children }) => {
         );
         console.log("response", response);
         if (response.status === 200) {
-          setBalance(newBalance);
+          fetchUpdateBalance();
         } else {
           console.error("Failed to update balance");
         }
@@ -66,99 +70,95 @@ const AccBalance: React.FC<AccBalanceProps> = ({ children }) => {
   function handleAddacc() {}
   return (
     <div>
-      <ProfileContext.Provider value={{ balance, setBalance }}>
-        {children}
-        <div className="flex justify-center space-x-4">
-          <div className="m-1 p-1">Cash: {balance}</div>
+      <div className="flex justify-center space-x-4">
+        <div className="m-1 p-1">Cash: {balance}</div>
 
-          {isAdjustingBalance ? (
-            <div>
-              <input
-                type="text"
-                className="border-blue-300"
-                value={newBalance || ""}
-                onChange={(event) =>
-                  handleBalanceChange(Number(event.target.value))
-                }
-                onKeyDown={handleKeyPress}
-              />
-              <button
-                onClick={handleSaveBalance}
-                className="bg-blue-500 text-white rounded-sm p-1 m-1"
-              >
-                {" "}
-                Done{" "}
-              </button>
-            </div>
-          ) : (
+        {isAdjustingBalance ? (
+          <div>
+            <input
+              type="text"
+              className="border-blue-300"
+              value={newBalance || ""}
+              onChange={(event) =>
+                handleBalanceChange(Number(event.target.value))
+              }
+              onKeyDown={handleKeyPress}
+            />
             <button
-              onClick={handleBalance}
-              className="bg-slate-500 text-white rounded-md p-1 m-1"
+              onClick={handleSaveBalance}
+              className="bg-blue-500 text-white rounded-sm p-1 m-1"
             >
               {" "}
-              Adjust Balance{" "}
+              Done{" "}
             </button>
-          )}
-        </div>
-        {/* {
+          </div>
+        ) : (
+          <button
+            onClick={handleBalance}
+            className="bg-slate-500 text-white rounded-md p-1 m-1"
+          >
+            {" "}
+            Adjust Balance{" "}
+          </button>
+        )}
+      </div>
+      {/* {
         isDropdown? ()
       <button className="bg-blue-500 text-white rounded-sm p-1 m-1" onClick={toggleDropdown}>Add Account</button>} */}
-        <div className="flex flex-col">
-          <button
-            className="bg-slate-500 text-white rounded-md p-1 m-auto w-32"
-            onClick={toggleDropdown}
-          >
-            Add Account
-          </button>
-          {isDropdown && (
-            <div>
-              <div className="bg-white-500 text-black rounded-sm p-1 m-auto border-2 border-red-300 w-96">
-                {" "}
-                <ul>
-                  <li>
-                    <Link to="/addaccount">Manual Input </Link>
-                    <p className="font-serif text-sm text-slate-500">
-                      Update your account manually. You can import your
-                      transaction or connect the account to your bank later, if
-                      you wish.
-                    </p>
-                  </li>
-                  <li>
-                    <Link to="#">Bank Sync</Link>
-                    <p className="font-serif text-sm text-slate-500">
-                      Connect to your bank account. Synchronise your
-                      transactions to Wallet automatically.
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          )}
-        </div>
-        <div>
-          <button
-            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0 m-10"
-            onClick={toggleRecordDropdown}
-          >
-            +
-          </button>
-
-          {isNewRecordDown && (
-            <div>
-              <ul className="absolute bottom-20 right-24">
-                <Link to="/template">
-                  <li>Template</li>
-                </Link>
-                <Link to="/newRecord">
-                  <li>New Record</li>
-                </Link>
+      <div className="flex flex-col">
+        <button
+          className="bg-slate-500 text-white rounded-md p-1 m-auto w-32"
+          onClick={toggleDropdown}
+        >
+          Add Account
+        </button>
+        {isDropdown && (
+          <div>
+            <div className="bg-white-500 text-black rounded-sm p-1 m-auto border-2 border-red-300 w-96">
+              {" "}
+              <ul>
+                <li>
+                  <Link to="/addaccount">Manual Input </Link>
+                  <p className="font-serif text-sm text-slate-500">
+                    Update your account manually. You can import your
+                    transaction or connect the account to your bank later, if
+                    you wish.
+                  </p>
+                </li>
+                <li>
+                  <Link to="#">Bank Sync</Link>
+                  <p className="font-serif text-sm text-slate-500">
+                    Connect to your bank account. Synchronise your transactions
+                    to Wallet automatically.
+                  </p>
+                </li>
               </ul>
             </div>
-          )}
-        </div>
-      </ProfileContext.Provider>
+          </div>
+        )}
+      </div>
+      <div>
+        <button
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0 m-10"
+          onClick={toggleRecordDropdown}
+        >
+          +
+        </button>
+
+        {isNewRecordDown && (
+          <div>
+            <ul className="absolute bottom-20 right-24">
+              <Link to="/template">
+                <li>Template</li>
+              </Link>
+              <Link to="/newRecord">
+                <li>New Record</li>
+              </Link>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
-export { ProfileContext };
 export default AccBalance;
