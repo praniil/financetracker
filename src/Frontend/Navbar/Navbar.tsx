a539e91bcc16eaf2cd4751f5cb804f4cb681c43f
praniil <pparajuli15@gmail.com>
2023-11-29 17:58:06 +0545

    update: applied css in the navbar

--- src/Frontend/Navbar/Navbar.tsx
+++ src/Frontend/Navbar/Navbar.tsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
+    <div className="bg-gray-800 py-2">
+      <nav className="flex items-center justify-center">
+        <Link
+          to="/"
+          className="text-white font-semibold px-4 py-1 rounded-md hover:bg-gray-700"
+        >
+          Home
+        </Link>
       </nav>
     </div>
   );
