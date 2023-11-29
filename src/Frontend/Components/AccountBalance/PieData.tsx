31f01c1b0264286beb3162ae95348df9aa476ddc
praniil <pparajuli15@gmail.com>
2023-11-29 16:43:43 +0545

    feat: added a function that toggles and shows the total expense

--- src/Frontend/Components/AccountBalance/PieData.tsx
+++ src/Frontend/Components/AccountBalance/PieData.tsx
@@ -32,11 +32,36 @@ const PieData: React.FC<props> = ({ passPieField, passPieData }) => {
     labels: databaseField,
     datasets: [databaseData],
   };
+  const [expenditure, setExpenditure] = useState<number>(0);
+  const [showExpenditure, setShowExpenditure] = useState<boolean>(false);
+
+  let totalExpenditure = 0;
+  for (let i = 0; i < databaseData.data.length; i++) {
+    totalExpenditure = totalExpenditure + databaseData.data[i];
+  }
+  function toggleExpenditure() {
+    setShowExpenditure(!showExpenditure);
+    setExpenditure(totalExpenditure);
+  }
 
   return (
-    <div className="w-96 h-96">
-      hey
+    <div className="w-96 h-96 m-auto pt-10 flex flex-col">
       <Pie data={data} />
+      {showExpenditure ? (
+        <div className="flex mt-2"> 
+          <div className=" text-sm  rounded-sm text-black bg-white-500 p-1 m-auto ">
+            Total: {expenditure}
+          </div>
+          <button className="text-xs rounded-sm text-white bg-blue-500 p-1 m-auto" onClick={toggleExpenditure}> Hide </button>
+        </div>
+      ) : (
+        <button
+          className="text-xs rounded-sm text-white bg-blue-500 p-1 m-4"
+          onClick={toggleExpenditure}
+        >
+          Show Total Expenditure
+        </button>
+      )}
     </div>
   );
 };
