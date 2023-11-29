f4235e92ebd7cbdedf318c24c642c3b410610e55
praniil <pparajuli15@gmail.com>
2023-11-29 11:08:20 +0545

    feat: added a fetchdata function to fetch the data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,6 +9,7 @@ import Navbar from "./Frontend/Navbar/Navbar";
 import Template from "./Frontend/Components/AccountBalance/Template";
 import NewRecord from "./Frontend/Components/AccountBalance/NewRecord";
 import PieData from "./Frontend/Components/AccountBalance/PieData";
+import axios from "axios";
 
 function App() {
   interface newRecord {
@@ -39,6 +40,30 @@ function App() {
     data: [],
     backgroundColor: [getRandomColor()],
   });
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      // Fetch databaseField and databaseData from the API
+      const response = await axios.get("http://localhost:8080/api/get-field");
+      const updateFieldData: pieData = response.data.data;
+
+      const keys = Object.keys(updateFieldData) as string[];
+      const values = Object.values(updateFieldData).flat() as string[];
+      setDatabaseField(keys);
+      setDatabaseData((prevData) => ({
+        ...prevData,
+        data: values.map((val) => parseInt(val, 10)), // Convert strings to numbers
+        backgroundColor: [...prevData.backgroundColor, getRandomColor()],
+      }));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <BrowserRouter>
       <div>
